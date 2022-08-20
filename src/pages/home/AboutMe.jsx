17b074e682c2d4919b48baed0d6b755c89bb3e3f
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2022-08-20 12:37:08 +0200

    Ježková

--- src/pages/home/AboutMe.jsx
+++ src/pages/home/AboutMe.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import "./aboutMe.css"
-import name from "../../assets/svgs/name.svg"
+import { ReactComponent as Name } from "../../assets/svgs/name.svg"
 import photo from "../../assets/images/Image 1.png"
 import Card from 'react-animated-3d-card'
 import certificate from "../../assets/images/whm_cerftified_instructor_logo_01.png"
 import { Fade } from 'react-reveal'
 
-export default function AboutMe({data}) {
+export default function AboutMe({ data }) {
     return (
         <section className='about'>
             <div className='about__info'>
                 <div>
                     <Fade top delay={500}><h2 id='about'>{data.o_mne[0].text}</h2></Fade>
-                    <Fade top delay={500}><img className='about__author__name' src={name} alt="Zuzana Ježková" /></Fade>
+                    <Fade top delay={500}><Name className='about__author__name' />{/* <img className='about__author__name' src={name} alt="Zuzana Ježková" /> */}</Fade>
                 </div>
                 <Fade top delay={500}>
                     <div className='about__text'><p>{data.o_mne_popis[0].text}</p></div>
