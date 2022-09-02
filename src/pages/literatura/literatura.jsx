ad01c1a2c12cf15532b9dddbc977602ad491c6ca
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2022-09-02 14:05:57 +0200

    Odkazy

--- src/pages/literatura/literatura.jsx
+++ src/pages/literatura/literatura.jsx
@@ -6,15 +6,24 @@ import LiteraturaBlock from './LiteraturaBlock'
 import "../home/recenze.css"
 
 export default function Literatura() {
-    const literatura = usePrismicDocumentsByType("literatura")
+    const literatura = usePrismicDocumentsByType("literatura", { pageSize: 100 })
+    const odkazy = usePrismicDocumentsByType("odkazy", { pageSize: 100 })
     return (
         <div className='gallery'>
             <div>
                 <Fade left><h2 className='gallery__heading'>Literatura</h2></Fade>
             </div>
             <div className='recenze__list'>
-                {literatura[1].state === 'loaded' ? literatura[0].results.map(data => {
-                    return <LiteraturaBlock data={data} />
+                {literatura[1].state === 'loaded' ? literatura[0].results.map((data, i) => {
+                    return <LiteraturaBlock key={i} data={data} />
+                }) : null}
+            </div>
+            <div>
+                <Fade left><h2 className='gallery__heading'>Odkazy</h2></Fade>
+            </div>
+            <div className='recenze__list'>
+                {odkazy[1].state === 'loaded' ? odkazy[0].results.map((data, i) => {
+                    return <a key={i} href={odkazy[0]?.results[0]?.data.odkaz?.url} target={'_blank'}><LiteraturaBlock data={data} /></a>
                 }) : null}
             </div>
         </div>
