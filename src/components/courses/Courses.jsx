6294237b7a958dc1d8bbcc5628ffc38aaa44ac22
Vojtech Suchanek <suchavo3@cvut.cz>
2023-03-08 14:21:48 +0100

    date

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,41 +1,43 @@
-import { useAllPrismicDocumentsByType, usePrismicDocumentsByIDs, usePrismicDocumentsByType } from '@prismicio/react'
-import React from 'react'
-import Course from '../../components/course/Course'
-import "./courses.css"
-import { Fade } from 'react-reveal'
-import * as prismicH from '@prismicio/helpers';
+import {
+  useAllPrismicDocumentsByType,
+  usePrismicDocumentsByIDs,
+  usePrismicDocumentsByType,
+} from "@prismicio/react";
+import React from "react";
+import Course from "../../components/course/Course";
+import "./courses.css";
+import { Fade } from "react-reveal";
+import * as prismicH from "@prismicio/helpers";
 
 export default function Courses({ language, data }) {
-    const [courses] = useAllPrismicDocumentsByType("kurz", { lang: language.lang })
-    let courseListSorted = courses
-    // console.log(Date.now())
-    if (!courses)
-        return null
-    courseListSorted.sort((a, b) => {
-        // console.log(prismicH.asDate(a.data.datum_kurzu).getTime())
-        const dateA = prismicH.asDate(a.data.datum_kurzu).getTime()
-        const dateB = prismicH.asDate(b.data.datum_kurzu).getTime() || 0
-        if (dateA < dateB) {
-            return -1;
-        }
-        if (dateA > dateB) {
-            return 1;
-        }
+  const [courses, state] = useAllPrismicDocumentsByType("kurz", {
+    lang: language.lang,
+  });
+  let courseListSorted = courses;
+  // console.log(Date.now())
 
-        // names must be equal
-        return 0;
-    });
+  if (state.state !== "loaded") return null;
+  if (!courses) return null;
+  courseListSorted.sort((a, b) => {
     return (
-        <div className='courses'>
-            <Fade top delay={500}><h2 id='courses' className='courses__headding'>{data.kurzy[0].text}</h2></Fade>
-            <ul className='courses__list'>
-                {courseListSorted.map((course) => {
-                    //console.log(course)
-                    if (prismicH.asDate(course.data.datum_kurzu).getTime() > Date.now())
-                        return <Course key={course.id} course={course}></Course>
-                    return null
-                })}
-            </ul>
-        </div>
-    )
+      prismicH.asDate(a.data.datum_kurzu) - prismicH.asDate(b.data.datum_kurzu)
+    );
+  });
+  return (
+    <div className="courses">
+      <Fade top delay={500}>
+        <h2 id="courses" className="courses__headding">
+          {data.kurzy[0].text}
+        </h2>
+      </Fade>
+      <ul className="courses__list">
+        {courseListSorted.map((course) => {
+          //console.log(course)
+          if (prismicH.asDate(course.data.datum_kurzu)?.getTime() > Date.now())
+            return <Course key={course.id} course={course}></Course>;
+          return null;
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import ParallaxScene from "./parallax/ParallaxScene"
-import Courses from '../../components/courses/Courses'
-import Pilire from './Pilire'
-import Recenze from './Recenze'
-import AboutMe from './AboutMe'
-import { usePrismicDocumentByID } from '@prismicio/react'
+import React from "react";
+import ParallaxScene from "./parallax/ParallaxScene";
+import Courses from "../../components/courses/Courses";
+import Pilire from "./Pilire";
+import Recenze from "./Recenze";
+import AboutMe from "./AboutMe";
+import {
+  usePrismicDocumentByID,
+  usePrismicDocumentsByTag,
+} from "@prismicio/react";
 
 export default function Home({ language, url }) {
-    const page = usePrismicDocumentByID("YslmPxAAACIAXPpC", { lang: language.lang })
-    if (page[1].state === "loaded")
-        return (
-            <>
-
-                <ParallaxScene data={page[0].data} url={url} />
-                <Courses data={page[0].data} language={language} />
-                <Pilire data={page[0].data} />
-                <AboutMe data={page[0].data} />
-                <Recenze data={page[0].data} language={language} />
-            </>
-        )
-    return null
+  let page = usePrismicDocumentsByTag("landing_page", { lang: language.lang });
+  // page = page[0]?.results[0]
+//   const page1 = usePrismicDocumentByID("YslmPxAAACIAXPpC", {
+//     lang: language.lang,
+//   });
+//   console.log(page);
+//   console.log(page1);
+  // return "test"
+  if (page[1].state === "loaded")
+    return (
+      <>
+        <ParallaxScene data={page[0].results[0].data} url={url} />
+        <Courses data={page[0].results[0].data} language={language} />
+        <Pilire data={page[0].results[0].data} />
+        <AboutMe data={page[0].results[0].data} />
+        <Recenze data={page[0].results[0].data} language={language} />
+      </>
+    );
+  return null;
 }
