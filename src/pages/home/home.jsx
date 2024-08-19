117c78242de2f2eb3237c0cf26090c6e45811853
Vojtech Suchanek <suchavo3@cvut.cz>
2024-08-19 14:04:24 +0200

    letter

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,23 +4,25 @@ import Courses from "../../components/courses/Courses";
 import Pilire from "./Pilire";
 import Recenze from "./Recenze";
 import AboutMe from "./AboutMe";
-import {
-  usePrismicDocumentsByTag,
-} from "@prismicio/react";
+import { usePrismicDocumentsByTag } from "@prismicio/react";
 
 export default function Home({ language, url }) {
   let page = usePrismicDocumentsByTag("landing_page", { lang: language.lang });
   // page = page[0]?.results[0]
-//   const page1 = usePrismicDocumentByID("YslmPxAAACIAXPpC", {
-//     lang: language.lang,
-//   });
-//   console.log(page);
-//   console.log(page1);
+  //   const page1 = usePrismicDocumentByID("YslmPxAAACIAXPpC", {
+  //     lang: language.lang,
+  //   });
+  //   console.log(page);
+  //   console.log(page1);
   // return "test"
   if (page[1].state === "loaded")
     return (
       <>
-        <ParallaxScene data={page[0].results[0].data} url={url} language={language} />
+        <ParallaxScene
+          data={page[0].results[0].data}
+          url={url}
+          language={language}
+        />
         <Courses data={page[0].results[0].data} language={language} />
         <Pilire data={page[0].results[0].data} />
         <AboutMe data={page[0].results[0].data} />
